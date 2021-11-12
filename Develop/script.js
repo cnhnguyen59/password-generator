7aa4ce9274cdc2028ee18d82e63820e3ee85d2c7
Christine Nguyen <cnhnguyen59@gmail.com>
2021-11-11 20:29:05 -0500

    consolidated tthe generatePassword func but stacking if statements for the char string and then generating password and added validation system to ensure all user input conditions are met

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,19 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); //selects the button
 
 // Write password to the #password input
 function writePassword() {
   var len = window.prompt("Enter length of password between 8 and 128 characters: ")
+  if (len === null) {
+    return;
+  }
   while (len <= 7 || len >= 129){
     var len = window.prompt("Invalid Entry. Enter length of password between 8 and 128 characters: ")
+    if (len === null) {
+      return;
   }
-
+  }
   var upper = window.prompt("Include uppercase letters? (Y/N): ").toLowerCase()
   while (upper != "y" && upper!= "n" ){
     var upper = window.prompt("Invalid Entry. Include uppercase letters? (Y/N): ")
@@ -44,113 +50,53 @@ function generatePassword(len, upper, lower, num, special){
   var lowerChar = 'abcdefghijklmnopqrstuvwxyz'
   var numChar = '0123456789'
   var specChar = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  var char = ''
 
-  if (upper == 'y' && lower == 'y' && num == 'y' && special == 'y'){ // includes upper, lower, num, and special chars
-    var char = upperChar + lowerChar + numChar + specChar
-    
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    }
-    return password
+  console.log(upper)
+  console.log(lower)
+  console.log(num)
+  console.log(special)
 
-  } else if (upper == 'y' && lower == 'y' && num == 'y' && special == 'n'){ // includes upper, lower, num chars
-    var char = upperChar + lowerChar + numChar
-
-    for (let i = 0; i < len; i++) { 
-      password += char[Math.floor(Math.random() * char.length)]
-      console.log(password)
-    }
-    return password
-  } else if (upper == 'y' && lower == 'y' && num == 'n' && special == 'y'){ // includes upper, lower, spec chars
-    var char = upperChar + lowerChar + specChar
-
-    for(let i = 0; i < len; i++) { 
-      password += char[Math.floor(Math.random() * char.length)]
-      console.log(password)
-    }
-    return password
-  } else if (upper == 'y' && lower == 'y' && num == 'n' && special == 'n'){ // includes upper and lower chars
-    var char = upperChar + lowerChar 
-
-    for(let i = 0; i < len; i++) { 
-      password += char[Math.floor(Math.random() * char.length)]
-      console.log(password)
-    }
-    return password
-  } else if (upper == 'y' && lower == 'n' && num == 'y' && special == 'n'){ // includes upper and num chars
-    var char = upperChar + numChar
-
-    for(let i = 0; i < len; i++) { 
-      password += char[Math.floor(Math.random() * char.length)]
-      console.log(password)
-    }
-    return password
-  } else if (upper == 'y' && lower == 'n' && num == 'y' && special == 'n'){ // includes upper and spec chars
-    var char = upperChar + specChar
-
-    for(let i = 0; i < len; i++) { 
-      password += char[Math.floor(Math.random() * char.length)]
-      console.log(password)
-    }
-    return password
-  } else if (upper == 'y' && lower == 'n' &&  num =='n' && special == 'n'){ // includes upper chars
-    var char = upperChar
-
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    
-    }
-    return password
-  } else if (upper == 'n' && lower == 'y' && num == 'y' && special == 'y'){ // includes lower, num, and special char
-    var char = lowerChar + numChar + specChar
-
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    }
-    return password
-  } else if (upper == 'n' && lower == 'y' && num == 'y' && special == 'n'){ // includes lower and num char
-    var char = lowerChar + numChar
-
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    }
-    return password
-  } else if (upper == 'n' && lower == 'y' && num == 'n' && spec == 'y'){ // includes lower and spec char
-    var char = lowerChar + specChar
-
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    }
-    return password
-  } else if (upper == 'n' && lower == 'y' && num == 'n' && special == 'n'){ // includes lower char
-    var char = lowerChar 
-
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    }
-    return password
-  } else if (upper == 'n' && lower == 'n' && num == 'y' && special == 'y'){ // includes num and spec char
-    var char = numChar + specChar
-
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    }
-    return password
-  } else if (upper == 'n' && lower == 'n' && num == 'y' && special == 'n'){ // includes num char
-    var char = numChar
-
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    }
-    return password
-  } else if (upper == 'n' && lower == 'n' && num == 'n' && special == 'y'){ // includes spec char
-    var char = specChar
-
-    for(let i = 0; i < len; i++) {
-      password += char[Math.floor(Math.random() * char.length)]
-    }
-    return password
+  if (upper == 'y'){
+    char += upperChar
   }
-    
-}
+  if (lower == 'y') {
+    char += lowerChar
+  }
+  if (num == 'y') {
+    char += numChar
+  }
+  if (special == 'y') {
+    char += specChar
+  }
+
+  console.log(char)
+
+  for(let i = 0; i < len; i++) {
+    password += char[Math.floor(Math.random() * char.length)]
+  }
+
+  console.log(password)
+
+  var hasUpper = /[A-Z]/.test(password)
+  var hasLower = /[a-z]/.test(password)
+  var hasNum = /[0-9]/.test(password)
+  var hasSpec = /[ !"#$%&\'()*+,-.\/:;<=>?@[\\\]^_`{|}~]/.test(password)
+
+  console.log(hasSpec)
+  console.log(hasUpper)
+  console.log(hasLower)
+  console.log(hasNum)
+
+ if (hasUpper == false || hasLower == false || hasNum == false || hasSpec == false) {
+    generatePassword(len, upper, lower, num, special)
+  } else {
+    console.log(password)
+    return password
+  } 
+} 
+
+
+
+ 
 
